01d26489b40526dbafc18608c31e180947ed7038
shadab012s <mdshadab0012s@gmail.com>
2024-10-12 16:36:12 +0530

    organising comp& adding button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
 import reslist from "../utils/mockData";
+import { useState } from "react";
 const Body=()=>{
-    return(<div className="body">
-        <div className="search">search</div>
+
+    const [ListOfRestaurants,setListOfRestaurants]=useState(reslist);
+    return(
+    <div className="body">
+      
+
+        <div className="filter">
+            <button className="filter-btn" 
+            onClick={()=>
+            {
+                const filterdList=ListOfRestaurants.filter(
+                    (res)=>res.info.avgRating>4.2
+                );
+                setListOfRestaurants(filterdList);
+            }
+            }
+            >
+               Top rated  Restaurants
+            </button>
+        </div>
         <div className="restaurant-container">
        {/* <Resturantcard resData={reslist[0]}/>  */}
 
        {/* using map to pass reslist data */}
 
-       {reslist.map((restaurant)=>
+       {ListOfRestaurants.map((restaurant)=>
     (<RestaurantCard key={restaurant.info.id} resData={restaurant}/>))}
         
         
